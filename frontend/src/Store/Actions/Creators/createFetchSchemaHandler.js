001200712cd13cf149c072335b7d54b4d1297d7b
Mark McDowall <markus.mcd5@gmail.com>
2017-02-10 22:47:11 -0800

    v3 UI

---
+++ frontend/src/Store/Actions/Creators/createFetchSchemaHandler.js
@@ -0,0 +1,33 @@
+import $ from 'jquery';
+import { set } from '../baseActions';
+
+function createFetchSchemaHandler(section, url) {
+  return function(getState, payload, dispatch) {
+    dispatch(set({ section, isFetchingSchema: true }));
+
+    const promise = $.ajax({
+      url
+    });
+
+    promise.done((data) => {
+      dispatch(set({
+        section,
+        isFetchingSchema: false,
+        schemaPopulated: true,
+        schemaError: null,
+        schema: data
+      }));
+    });
+
+    promise.fail((xhr) => {
+      dispatch(set({
+        section,
+        isFetchingSchema: false,
+        schemaPopulated: true,
+        schemaError: xhr
+      }));
+    });
+  };
+}
+
+export default createFetchSchemaHandler;
